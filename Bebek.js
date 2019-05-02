3fc6521513554e54583883fa0fb361e083fd36b7
pimenkidis <arsyadiman@gmail.com>
2019-05-02 04:28:24 -0700

    addFile

---
+++ Bebek.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Bebek extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default Bebek;
+
+//rnss
